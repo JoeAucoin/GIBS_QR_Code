a26dfbddfeff341505105e4288a8822772c929d5
JoeAucoin <jaucoin@gibs.com>
2025-01-02 15:55:10 -0500

    Add project files.

---
+++ View.ascx
@@ -0,0 +1,162 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="View.ascx.cs" Inherits="GIBS.Modules.GIBS_QR_Code.View" %>
+<%@ Register TagPrefix="Portal" TagName="URL" Src="~/controls/URLControl.ascx" %>
+<%@ Register TagPrefix="dnn" TagName="Label" Src="~/controls/LabelControl.ascx" %>
+
+
+<script type="text/javascript">
+    function clearTextBox() {
+       // alert("here");
+        document.getElementById('<%= TextBoxEMAIL.ClientID %>').value = "";
+        document.getElementById('<%= TextBoxURL.ClientID %>').value = "";
+        document.getElementById('<%= TextBoxTEXT.ClientID %>').value = "";
+        // Get the divs
+        const hideURL = document.getElementById("divurl"); 
+        const hideTEXT = document.getElementById("divtext"); 
+        const hideEMAIL = document.getElementById("divemail"); 
+        hideURL.style.display = "none";
+        hideEMAIL.style.display = "none";
+        hideTEXT.style.display = "block";
+      //  alert("here2");
+    }
+
+    function clearURL() {
+        // alert("here");
+        document.getElementById('<%= TextBoxEMAIL.ClientID %>').value = "";
+        document.getElementById('<%= TextBoxURL.ClientID %>').value = "";
+        document.getElementById('<%= TextBoxTEXT.ClientID %>').value = "";
+        // Get the divs
+        const hideURL = document.getElementById("divurl");
+        const hideTEXT = document.getElementById("divtext");
+        const hideEMAIL = document.getElementById("divemail");
+        hideURL.style.display = "block";
+        hideEMAIL.style.display = "none";
+        hideTEXT.style.display = "none";
+        //  alert("here2");
+    }
+    function clearEMAIL() {
+        // alert("here");
+        document.getElementById('<%= TextBoxEMAIL.ClientID %>').value = "";
+            document.getElementById('<%= TextBoxURL.ClientID %>').value = "";
+            document.getElementById('<%= TextBoxTEXT.ClientID %>').value = "";
+            // Get the div element by its ID
+            const hideURL = document.getElementById("divurl");
+            const hideTEXT = document.getElementById("divtext");
+            const hideEMAIL = document.getElementById("divemail");
+            hideURL.style.display = "none";
+            hideEMAIL.style.display = "block";
+            hideTEXT.style.display = "none";
+            //  alert("here2");
+        }
+</script>
+
+<div>
+    <asp:DropDownList ID="ddlQRType" runat="server" OnSelectedIndexChanged="ddlQRType_SelectedIndexChanged" AutoPostBack="true">
+        <asp:ListItem Text="VCard" Value="VCard" Selected="True"></asp:ListItem>
+        <asp:ListItem Text="Other" Value="Other"></asp:ListItem>
+    </asp:DropDownList>
+</div>
+
+<asp:Panel ID="PanelVCard" runat="server">
+
+    <div class="dnnForm">
+        <fieldset>
+            <div class="dnnFormItem">
+                <dnn:label runat="server" ControlName="txtFirstName" Text="First Name" />
+                <asp:TextBox ID="txtFirstName" runat="server" />
+            </div>
+            <div class="dnnFormItem">
+                <dnn:label runat="server" ControlName="txtLastName" Text="Last Name" />
+                <asp:TextBox ID="txtLastName" runat="server" />
+            </div>	
+            <div class="dnnFormItem">
+                <dnn:label runat="server" ControlName="txtCellPhone" Text="Cell Phone" />
+                <asp:TextBox ID="txtCellPhone" runat="server" />
+            </div>
+        <div class="dnnFormItem">
+            <dnn:label runat="server" ControlName="txtWorkPhone" Text="Work Phone" />
+            <asp:TextBox ID="txtWorkPhone" runat="server" />
+        </div>	            
+            <div class="dnnFormItem">
+                <dnn:Label runat="server" ControlName="txtEmail" Text="Email" />
+                <asp:TextBox ID="txtEmail"  runat="server" />
+            </div>		
+            <div class="dnnFormItem">
+                <dnn:Label runat="server" ControlName="txtStreet" Text="Street" />
+                <asp:TextBox ID="txtStreet"  runat="server" />
+            </div>
+            <div class="dnnFormItem">
+                <dnn:Label runat="server" ControlName="txtCity" Text="City" />
+                <asp:TextBox ID="txtCity"  runat="server" />
+            </div>
+
+            <div class="dnnFormItem">
+                <dnn:Label runat="server" ControlName="txtState" Text="State" />
+                <asp:TextBox ID="txtState"  runat="server" />
+            </div>
+ 
+            <div class="dnnFormItem">
+                <dnn:Label runat="server" ControlName="txtZip" Text="Zip" />
+                <asp:TextBox ID="txtZip"  runat="server" />
+            </div>	
+            <div class="dnnFormItem">
+                <dnn:Label runat="server" ControlName="txtWebsite" Text="Website" />
+                <asp:TextBox ID="txtWebsite"  runat="server" />
+            </div>
+            
+        <div class="dnnFormItem">
+            <dnn:Label runat="server" ControlName="txtBusiness" Text="Company" />
+            <asp:TextBox ID="txtBusiness"  runat="server" />
+        </div>	
+        <div class="dnnFormItem">
+            <dnn:Label runat="server" ControlName="txtTitle" Text="Title" />
+            <asp:TextBox ID="txtTitle"  runat="server" />
+        </div>	
+        </fieldset>
+    </div>
+
+</asp:Panel>
+
+<asp:Panel ID="PanelOther" runat="server" Visible="false">
+
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link" aria-current="page" href="#" onclick="javascript:clearURL();">URL</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#" onclick="javascript:clearTextBox();">Text</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#" onclick="javascript:clearEMAIL();">Email</a>
+  </li>
+
+</ul>
+
+
+
+<div class="dnnForm">
+    <fieldset>
+        <div class="dnnFormItem" id="divurl">
+            <dnn:label runat="server" ControlName="TextBoxURL" Text="URL" HelpText="Enter URL" />
+            <asp:TextBox ID="TextBoxURL" runat="server" CliendIDMode="Static" />
+        </div>
+        <div class="dnnFormItem" id="divtext" style="display:none;">
+            <dnn:Label runat="server" ControlName="TextBoxTEXT" Text="Text" HelpText="Enter Any Text" />
+            <asp:TextBox ID="TextBoxTEXT"  runat="server" TextMode="MultiLine" CssClass="formattextbox" CliendIDMode="Static" />
+        </div>
+        <div class="dnnFormItem" id="divemail" style="display:none;">
+            <dnn:Label runat="server" ControlName="TextBoxEMAIL" Text="E-Mail" HelpText="Enter An E-Mail Address" />
+            <asp:TextBox ID="TextBoxEMAIL" runat="server" CliendIDMode="Static" />
+        </div>	
+    </fieldset>
+</div>
+</asp:Panel>
+
+
+
+<div class="nav justify-content-center"><asp:LinkButton ID="LinkButtonSUBMIT" runat="server" OnClick="LinkButtonSUBMIT_Click" CssClass="btn btn-lg btn-default">Create QR Code</asp:LinkButton></div>
+
+<asp:Label ID="LabelEncodedString" runat="server" Text="" Visible="false"></asp:Label>
+<br />
+<asp:Image ID="Image1" runat="server" Visible="false" Width="300px" />
+
+
---
+++ View.ascx.cs
@@ -0,0 +1,231 @@
+﻿/*
+' Copyright (c) 2024  GIBS.com
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Modules.Actions;
+using DotNetNuke.Security;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Services.Localization;
+using QRCoder;
+using System;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Drawing;
+using static QRCoder.PayloadGenerator;
+
+namespace GIBS.Modules.GIBS_QR_Code
+{
+    /// -----------------------------------------------------------------------------
+    /// <summary>
+    /// The View class displays the content
+    /// 
+    /// Typically your view control would be used to display content or functionality in your module.
+    /// 
+    /// View may be the only control you have in your project depending on the complexity of your module
+    /// 
+    /// Because the control inherits from GIBS_QR_CodeModuleBase you have access to any custom properties
+    /// defined there, as well as properties from DNN such as PortalId, ModuleId, TabId, UserId and many more.
+    /// 
+    /// </summary>
+    /// -----------------------------------------------------------------------------
+    public partial class View : GIBS_QR_CodeModuleBase, IActionable
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!IsPostBack)
+                {
+                    ////    TextBox6.Text = GenerateAndGetString();
+
+                    //    QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                    //    QRCodeData qrCodeData = qrGenerator.CreateQrCode(TextBoxTEXT.Text.ToString(), QRCodeGenerator.ECCLevel.Q);
+                    //    QRCode qrCode = new QRCode(qrCodeData);
+
+                    //    Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+                    //    using (Bitmap bitMap = qrCode.GetGraphic(20))
+                    //    {
+                    //        using (MemoryStream ms = new MemoryStream())
+                    //        {
+                    //            Image1.Visible = true;
+                    //            bitMap.Save(ms, ImageFormat.Png);
+                    //            byte[] byteImage = ms.ToArray();
+                    //            Image1.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+                    //        }
+                    //    }
+                    //}
+                }
+            }
+            catch (Exception exc) //Module failed to load
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+        public ModuleActionCollection ModuleActions
+        {
+            get
+            {
+                var actions = new ModuleActionCollection
+                    {
+                        {
+                            GetNextActionID(), Localization.GetString("EditModule", LocalResourceFile), "", "", "",
+                            EditUrl(), false, SecurityAccessLevel.Edit, true, false
+                        }
+                    };
+                return actions;
+            }
+        }
+
+        protected void LinkButtonSUBMIT_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (ddlQRType.SelectedValue == "Other")
+                {
+                    SaveOther();
+                }
+                else
+                {
+                    SaveVCard();
+                }
+
+            }
+            catch (Exception exc) //Module failed to load
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+
+        }
+
+        // VGV4dEJveFRFWFQuVGV4dC5Ub1N0cmluZygp
+
+
+        public void SaveVCard()
+        {
+            try
+            {
+                ContactData generator = new ContactData(ContactData.ContactOutputType.VCard3, txtFirstName.Text.ToString(), txtLastName.Text.ToString(),
+                    "", "", txtCellPhone.Text.ToString(), txtWorkPhone.Text.ToString(), txtEmail.Text.ToString(), null,txtWebsite.Text.ToString(),txtStreet.
+                    Text.ToString(),null,txtCity.Text.ToString(), txtZip.Text.ToString(), null, null, txtState.Text.ToString(),ContactData.AddressOrder.Reversed,
+                    txtBusiness.Text.ToString(), txtTitle.Text.ToString());
+                string payload = generator.ToString();
+
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                // QRCodeData qrCodeData = qrGenerator.CreateQrCode(TextBoxTEXT.Text.ToString(), QRCodeGenerator.ECCLevel.Q);
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+
+                Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+                using (Bitmap bitMap = qrCode.GetGraphic(20))
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        Image1.Visible = true;
+                        bitMap.Save(ms, ImageFormat.Png);
+                        byte[] byteImage = ms.ToArray();
+                        Image1.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+
+                    }
+                }
+
+                string myEncodedString = Base64Encode(TextBoxTEXT.Text.ToString());
+                LabelEncodedString.Text = payload.ToString();
+                //    LabelEncodedString.Text = TextBoxTEXT.Text.Replace(Environment.NewLine, "<br />").ToString();
+
+
+            }
+            catch (Exception ex)
+            {
+                Exceptions.ProcessModuleLoadException(this, ex);
+            }
+        }
+
+
+
+        public void SaveOther()
+        {
+            try
+            {
+                string email = TextBoxEMAIL.Text.ToString();
+
+                if (email.Length > 0)
+                {
+                    email = "mailto:" + email;
+                }
+
+
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(TextBoxURL.Text.ToString()+ email.ToString() + TextBoxTEXT.Text.ToString(), QRCodeGenerator.ECCLevel.Q);
+                
+                QRCode qrCode = new QRCode(qrCodeData);
+
+                Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+                using (Bitmap bitMap = qrCode.GetGraphic(20))
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        Image1.Visible = true;
+                        bitMap.Save(ms, ImageFormat.Png);
+                        byte[] byteImage = ms.ToArray();
+                        Image1.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+
+                    }
+                }
+
+                string myEncodedString = Base64Encode(TextBoxTEXT.Text.ToString());
+               
+                LabelEncodedString.Text = TextBoxTEXT.Text.Replace(Environment.NewLine, "<br />").ToString();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Exceptions.ProcessModuleLoadException(this, ex);
+            }
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+
+
+        protected void ddlQRType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(ddlQRType.SelectedValue == "Other")
+            { 
+                PanelOther.Visible = true;
+                PanelVCard.Visible = false;
+            }
+            else
+            {
+                PanelOther.Visible = false;
+                PanelVCard.Visible = true;
+            }
+        }
+    }
+}
